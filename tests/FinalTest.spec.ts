d023357f326afbddf36cbc204739324a89fe48e0
Alexander_Kost <alexander.kostarov@gmail.com>
2025-02-12 15:53:42 +0400

    try 1 step 1

---
+++ tests/FinalTest.spec.ts
@@ -0,0 +1,12 @@
+import { test } from "@playwright/test";
+
+test("Step 1 - login to mail", async ({ page }) => {
+    await page.goto("https://mailfence.com/");
+    await page.locator("#signin").click();
+    // await page.getByText('Log in').click()
+    await page.locator("#UserID").fill("alik");
+    await page.locator("#Password").fill("sEepqTaxB8P5@qw");
+    await page.locator('[value="Enter"]').click();
+});
+
+// alik sEepqTaxB8P5@qw
